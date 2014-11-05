953c36da1252b2ddebffffeb4484543ae89be55c
Joe Germuska <joe@germuska.com>
2014-11-05 17:16:21 -0600

    committing from Xcode.

--- helloworld/ViewController.swift
+++ helloworld/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    @IBOutlet var toolbar: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let requestURL = NSURL(string: "http://knightlab.northwestern.edu")
+        let request = NSURLRequest(URL: requestURL!)
+        webView.loadRequest(request)
     }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setToolbarHidden(false, animated: animated)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func googleClicked(sender: AnyObject) {
+        let requestURL = NSURL(string: "http://google.com")
+        let request = NSURLRequest(URL: requestURL!)
+        webView.loadRequest(request)
+        self.view.backgroundColor = UIColor.lightGrayColor()
+        
+    }
+    
+    @IBAction func nytClicked(sender: AnyObject) {
+        let requestURL = NSURL(string: "http://nytimes.com")
+        let request = NSURLRequest(URL: requestURL!)
+        webView.loadRequest(request)
+        self.view.backgroundColor = UIColor.magentaColor()
+        
+    }
 
+    @IBAction func redditClicked(sender: AnyObject) {
+        let requestURL = NSURL(string: "http://reddit.com")
+        let request = NSURLRequest(URL: requestURL!)
+        webView.loadRequest(request)
+        self.view.backgroundColor = UIColor.greenColor()
+        
+    }
 }
 
